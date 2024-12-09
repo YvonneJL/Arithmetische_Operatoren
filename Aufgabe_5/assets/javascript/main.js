3cdaeee69c979dfe931351a8aa69f08cbf3177c5
YvonneJL <yvonneloder@gmx.de>
2024-12-09 14:52:24 +0100

    fertig

---
+++ Aufgabe_5/assets/javascript/main.js
@@ -0,0 +1,53 @@
+let x = 20;
+let y = 30;
+
+
+let sum = x + y;
+console.log(sum);
+
+let difference = x - y;
+console.log(difference);
+
+let differenceTwo = y - x;
+console.log(differenceTwo);
+
+let product = x * y;
+console.log(product);
+
+let quotient = x / y;
+console.log(quotient);
+
+let z = 10;
+
+let productQuotient = x * y / z;
+console.log(productQuotient);
+
+let resultOne = 60;
+
+let a = 15;
+let b = 9;
+
+console.log(a % b);
+
+let c = 20;
+console.log((a + b) * c);
+
+
+let resultTwo = 480;
+console.log(resultTwo);
+
+
+a ++
+console.log(a);
+
+b --
+console.log(b);
+
+let resultThree = a - b;
+console.log(resultThree);
+
+let rest = resultOne % resultTwo;
+console.log(rest);
+
+
+console.log(60 % 480);
---
+++ Aufgabe_6/assets/javascript/main.js
@@ -0,0 +1,28 @@
+let score = 5 + 5 * 10;
+console.log(score);
+
+score = (5 + 5) * 10;
+console.log(score);
+
+score = 10;
+score = score +10;
+console.log(score);
+
+// !Oben überschreibe ich und nutze dafür sogar den alten score Wert...
+
+score += 10;
+console.log(score);
+
+// !Oben: Zuweisungsoperator also score von davor (20) plus 10
+
+let y = 1;
+y = y + 1;
+y += 1;
+
+console.log(y);
+
+y++
+console.log(y);
+
+y--
+console.log(y);
